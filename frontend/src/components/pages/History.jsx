c9d796a73ed740d8a736ac31dcd24b5b2f241a7f
pgalkevich <pgalkevich@gmail.com>
2022-05-04 21:26:43 +0300

    infinite scroll

--- frontend/src/components/pages/History.jsx
+++ frontend/src/components/pages/History.jsx
@@ -1,19 +1,80 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import Store from "../../store/store";
 import {observer} from "mobx-react";
 import {Loader} from "../Loader/Loader";
 import {Row} from "../Row/Row";
-import {runInAction} from "mobx";
 
 
 export const History = observer(() => {
     let store = Store;
     const {groupedEvents} = store;
     const groups = Object.keys(groupedEvents || {});
+    const [displayedItems, setDisplayedItems] = useState(100);
+    const [stopRender, setStopRender] = useState(false);
+
+    const handleScrollEvent = useCallback((e) => {
+        const docHeight = e.target.documentElement.scrollHeight;
+        const scrolledValue = e.target.documentElement.scrollTop;
+        const windowHeight = window.innerHeight;
+
+        if (stopRender) {
+            return;
+        }
+
+        if (docHeight - (windowHeight + scrolledValue) < 100) {
+            setDisplayedItems((prevCount) => prevCount + 100);
+        }
+    }, [stopRender]);
 
     useEffect(() => {
-        return () => store = null;
-    }, []);
+        document.addEventListener('scroll', handleScrollEvent);
+        return () => {
+            store = null;
+            document.removeEventListener('scroll', handleScrollEvent);
+        }
+    }, [displayedItems]);
+
+    useEffect(() => {
+        if (!!store && !!store.events && (store.events.length <= displayedItems)) {
+            setStopRender(true);
+        }
+    }, [displayedItems, store.events, store]);
+
+    const renderRows = useMemo(() => {
+        let localCount = 1;
+        const elements = [];
+
+        for (let group of groups) {
+            if (localCount === displayedItems) {
+                break;
+            }
+
+            elements.push(<div key={group} className={'event-group'}>
+                {
+                    groupedEvents[group]
+                        .map((evt, i) =>  {
+                            if (localCount === displayedItems) {
+                                return null;
+                            }
+                            localCount++;
+                            return <Row
+                                key={evt.id}
+                                eventType={!i ? evt.resource : ''}
+                                details={
+                                    store.resources && store.resources[`${evt.resource}/${evt.id}`].details
+                                }
+                                code={
+                                    store.resources && store.resources[`${evt.resource}/${evt.id}`].values &&
+                                    store.resources[`${evt.resource}/${evt.id}`].values.join(', ')}
+                                date={evt.date}
+                            />;
+                        })
+                }
+            </div>);
+        }
+
+        return elements;
+    }, [displayedItems, groupedEvents, store.resources]);
 
 
     return (
@@ -24,26 +85,7 @@ export const History = observer(() => {
                     <div className={'event-group'}>
                         <Row header />
                     </div>
-                    {
-                        groups.map(group => <div key={group} className={'event-group'}>
-                            {
-                                groupedEvents[group]
-                                    .slice(0,15)
-                                    .map((evt, i) => <Row
-                                        key={evt.id}
-                                        eventType={!i ? evt.resource : ''}
-                                        details={
-                                            store.resources && store.resources[`${evt.resource}/${evt.id}`].details
-                                        }
-                                        code={
-                                            store.resources && store.resources[`${evt.resource}/${evt.id}`].values &&
-                                            store.resources[`${evt.resource}/${evt.id}`].values.join(', ')}
-                                        date={evt.date}
-                                    />)
-                            }
-                        </div>
-                        )
-                    }
+                    {renderRows}
                 </>
             )}
         </div>
